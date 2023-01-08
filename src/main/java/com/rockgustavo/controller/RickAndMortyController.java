382984626724f94b0ab8180e22660159855d3b7e
rockgustavo <grcuritiba@gmail.com>
2023-01-08 17:29:58 -0300

    v1.1 08-01-2023

--- src/main/java/com/rockgustavo/controller/RickAndMortyController.java
+++ src/main/java/com/rockgustavo/controller/RickAndMortyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.rockgustavo.model.Episode;
 import com.rockgustavo.model.RickAndMorty;
 import com.rockgustavo.service.RickAndMortyService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class RickAndMortyController {
